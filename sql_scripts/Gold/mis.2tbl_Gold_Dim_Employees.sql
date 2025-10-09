781630c8f275b03f0052303f5318f92f8f42d1dd
Eugeniu Pascal <eugeniu.pascal@microinvest.md>
2025-10-09 12:42:25 +0300

    CommitUntracked

--- sql_scripts/Gold/mis.2tbl_Gold_Dim_Employees.sql
+++ sql_scripts/Gold/mis.2tbl_Gold_Dim_Employees.sql
@@ -8,7 +8,9 @@ GO
 CREATE TABLE mis.[2tbl_Gold_Dim_Employees] (
     [EmployeeID] VARCHAR(36) NOT NULL,
     [EmployeeCode] INT NULL,
+    [EmployeePositionID] VARCHAR(36) NULL,
     [EmployeeName] NVARCHAR(40) NULL,
+    [EmployeePosition] NVARCHAR(150) NULL,
     [HireDate] NVARCHAR(10) NULL,
     [BirthDate] NVARCHAR(10) NULL,
     [DismissalDate] NVARCHAR(10) NULL,
@@ -16,17 +18,19 @@ CREATE TABLE mis.[2tbl_Gold_Dim_Employees] (
     [ExperienceYears] INT NULL,
     [ExperienceMonths] INT NULL,
     [ExperienceYM] NVARCHAR(50) NULL,
-    [EmploymentPeriod] NVARCHAR(50) NULL,
-    [EmployeePositionID] VARCHAR(36) NULL,
-    [EmployeePosition] NVARCHAR(150) NULL,
-    [ExperienceYearIndex] INT NULL
+	[ExperienceMonthsRange] NVARCHAR(50) NULL,
+    [ExperienceIndex] INT NULL,
+    [EmploymentPeriod] NVARCHAR(50) NULL
 );
 GO
 
-INSERT INTO mis.[2tbl_Gold_Dim_Employees] (
+INSERT INTO mis.[2tbl_Gold_Dim_Employees] 
+(
     [EmployeeID],
     [EmployeeCode],
+    [EmployeePositionID],
     [EmployeeName],
+    [EmployeePosition],
     [HireDate],
     [BirthDate],
     [DismissalDate],
@@ -34,52 +38,41 @@ INSERT INTO mis.[2tbl_Gold_Dim_Employees] (
     [ExperienceYears],
     [ExperienceMonths],
     [ExperienceYM],
-    [EmploymentPeriod],
-    [EmployeePositionID],
-    [EmployeePosition],
-    [ExperienceYearIndex]
+	[ExperienceMonthsRange],
+    [ExperienceIndex],
+    [EmploymentPeriod]  
 )
 SELECT 
     a.[Сотрудники ID] AS EmployeeID,
     a.[Сотрудники Код] AS EmployeeCode,
+    lastPos.[СотрудникиДанныеПоЗарплате Должность ID] AS EmployeePositionID,
     a.[Сотрудники Наименование] AS EmployeeName,
-    
+    lastPos.[СотрудникиДанныеПоЗарплате Должность] AS EmployeePosition,
+
     -- HireDate
     CASE 
         WHEN a.[Сотрудники Дата Приема] IS NULL OR a.[Сотрудники Дата Приема] = '1753-01-01'
         THEN N'N/A'
         ELSE FORMAT(a.[Сотрудники Дата Приема], 'yyyy-MM-dd')
     END AS HireDate,
-    
+
     -- BirthDate
     CASE 
         WHEN a.[Сотрудники Дата Рождения] IS NULL OR a.[Сотрудники Дата Рождения] = '1753-01-01'
         THEN N'N/A'
         ELSE FORMAT(a.[Сотрудники Дата Рождения], 'yyyy-MM-dd')
     END AS BirthDate,
-    
+
     -- DismissalDate
     CASE 
         WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
         THEN N'N/A'
         ELSE FORMAT(a.[Сотрудники Дата Увольнения], 'yyyy-MM-dd')
     END AS DismissalDate,
-    
+
     a.[Сотрудники Табельный Номер] AS TimesheetNumber,
 
-    -- ExperienceYears
-    CASE 
-        WHEN a.[Сотрудники Дата Приема] IS NULL OR a.[Сотрудники Дата Приема] = '1753-01-01'
-        THEN NULL
-        ELSE DATEDIFF(YEAR, a.[Сотрудники Дата Приема], 
-             CASE 
-                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
-                 THEN GETDATE()
-                 ELSE a.[Сотрудники Дата Увольнения]
-             END)
-    END AS ExperienceYears,
-
-    -- ExperienceMonths (months leftover after full years)
+    -- ExperienceYears as INT
     CASE 
         WHEN a.[Сотрудники Дата Приема] IS NULL OR a.[Сотрудники Дата Приема] = '1753-01-01'
         THEN NULL
@@ -88,26 +81,81 @@ SELECT
                  WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
                  THEN GETDATE()
                  ELSE a.[Сотрудники Дата Увольнения]
-             END) % 12
+             END) / 12
+    END AS ExperienceYears,
+
+    -- Total months worked
+    CASE 
+        WHEN a.[Сотрудники Дата Приема] IS NULL OR a.[Сотрудники Дата Приема] = '1753-01-01'
+        THEN NULL
+        ELSE DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE 
+                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
+                 THEN GETDATE()
+                 ELSE a.[Сотрудники Дата Увольнения]
+             END)
     END AS ExperienceMonths,
 
     -- ExperienceYM
     CASE 
         WHEN a.[Сотрудники Дата Приема] IS NULL OR a.[Сотрудники Дата Приема] = '1753-01-01'
         THEN N'N/A'
-        ELSE CAST(DATEDIFF(YEAR, a.[Сотрудники Дата Приема], 
-                 CASE 
-                     WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
-                     THEN GETDATE()
-                     ELSE a.[Сотрудники Дата Увольнения]
-                 END) AS NVARCHAR(3)) + N' years ' +
-             CAST(DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
-                 CASE 
-                     WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
-                     THEN GETDATE()
-                     ELSE a.[Сотрудники Дата Увольнения]
-                 END) % 12 AS NVARCHAR(2)) + N' months'
+        ELSE 
+            CAST(DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+                 CASE WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01' 
+                      THEN GETDATE() ELSE a.[Сотрудники Дата Увольнения] END) / 12 AS NVARCHAR(3)) 
+            + N' years ' + 
+            CAST(DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+                 CASE WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01' 
+                      THEN GETDATE() ELSE a.[Сотрудники Дата Увольнения] END) % 12 AS NVARCHAR(2)) 
+            + N' months'
     END AS ExperienceYM,
+	
+	-- Add ExperienceMonthsRange
+    CASE 
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE 
+                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
+                 THEN GETDATE()
+                 ELSE a.[Сотрудники Дата Увольнения]
+             END) BETWEEN 1 AND 5 THEN N'1-5 m'
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE 
+                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
+                 THEN GETDATE()
+                 ELSE a.[Сотрудники Дата Увольнения]
+             END) BETWEEN 6 AND 11 THEN N'6-11 m'
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE 
+                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
+                 THEN GETDATE()
+                 ELSE a.[Сотрудники Дата Увольнения]
+             END) BETWEEN 12 AND 35 THEN N'12-35 m'
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE 
+                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
+                 THEN GETDATE()
+                 ELSE a.[Сотрудники Дата Увольнения]
+             END) > 35 THEN N'36+ m'
+    ELSE N'N/A'
+END AS ExperienceMonthsRange,
+
+-- ExperienceIndex
+   CASE 
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01' 
+               THEN GETDATE() ELSE a.[Сотрудники Дата Увольнения] END) BETWEEN 1 AND 5 THEN 1
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01' 
+               THEN GETDATE() ELSE a.[Сотрудники Дата Увольнения] END) BETWEEN 6 AND 11 THEN 2
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01' 
+               THEN GETDATE() ELSE a.[Сотрудники Дата Увольнения] END) BETWEEN 12 AND 35 THEN 3
+        WHEN DATEDIFF(MONTH, a.[Сотрудники Дата Приема], 
+             CASE WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01' 
+               THEN GETDATE() ELSE a.[Сотрудники Дата Увольнения] END) > 35 THEN 4
+        ELSE NULL
+    END AS ExperienceIndex,
 
     -- EmploymentPeriod
     CASE 
@@ -116,40 +164,7 @@ SELECT
         WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
         THEN FORMAT(a.[Сотрудники Дата Приема], 'yyyy-MM-dd') + N' → Present'
         ELSE FORMAT(a.[Сотрудники Дата Приема], 'yyyy-MM-dd') + N' → ' + FORMAT(a.[Сотрудники Дата Увольнения], 'yyyy-MM-dd')
-    END AS EmploymentPeriod,
-
-    -- Employee Position
-    lastPos.[СотрудникиДанныеПоЗарплате Должность ID] AS EmployeePositionID,
-    lastPos.[СотрудникиДанныеПоЗарплате Должность] AS EmployeePosition,
-
-    -- ExperienceYearIndex
-    CASE 
-        WHEN DATEDIFF(YEAR, a.[Сотрудники Дата Приема], 
-             CASE 
-                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
-                 THEN GETDATE()
-                 ELSE a.[Сотрудники Дата Увольнения]
-             END) < 1 THEN 0
-        WHEN DATEDIFF(YEAR, a.[Сотрудники Дата Приема], 
-             CASE 
-                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
-                 THEN GETDATE()
-                 ELSE a.[Сотрудники Дата Увольнения]
-             END) BETWEEN 1 AND 20 THEN 1
-        WHEN DATEDIFF(YEAR, a.[Сотрудники Дата Приема], 
-             CASE 
-                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
-                 THEN GETDATE()
-                 ELSE a.[Сотрудники Дата Увольнения]
-             END) BETWEEN 21 AND 30 THEN 2
-        WHEN DATEDIFF(YEAR, a.[Сотрудники Дата Приема], 
-             CASE 
-                 WHEN a.[Сотрудники Дата Увольнения] IS NULL OR a.[Сотрудники Дата Увольнения] = '1753-01-01'
-                 THEN GETDATE()
-                 ELSE a.[Сотрудники Дата Увольнения]
-             END) BETWEEN 31 AND 40 THEN 3
-        ELSE 4
-    END AS ExperienceYearIndex
+    END AS EmploymentPeriod
 
 FROM [ATK].[dbo].[Справочники.Сотрудники] AS a
 OUTER APPLY (

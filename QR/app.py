bcb72eca208d3b5578a9e2eca61d57b2a4535702
Eugeniu Pascal <eugeniu.pascal@microinvest.md>
2025-12-17 08:26:20 +0200

    commitChanges

--- QR/app.py
+++ QR/app.py
@@ -173,4 +173,6 @@ if __name__ == "__main__":
         print("❌ Failed to get access token")
 
     # Start Flask app
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)

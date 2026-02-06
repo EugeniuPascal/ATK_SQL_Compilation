b26cf5e187585ab26b7cfd940195bb533786a9d7
Eugeniu Pascal <eugeniu.pascal@microinvest.md>
2026-02-06 16:58:27 +0200

    commitStaged

--- compiled/compiled_silver_job_proc.sql
+++ compiled/compiled_silver_job_proc.sql
@@ -1,6 +1,6 @@
 ﻿-- =============================================
 -- Compiled Stored Procedure for MSSQL Agent Job (Silver) - Idempotent
--- Generated: 2026-02-06 16:09:48.375787
+-- Generated: 2026-02-06 16:51:45.953876
 -- Source folder: C:\ATK_Project\sql_scripts\Silver
 -- Files included: 12
 --   mis.Silver_CerereOnline_base.sql
--- compiled/manual_run/compiled_Silver_Tables.sql
+++ compiled/manual_run/compiled_Silver_Tables.sql
@@ -1,5 +1,5 @@
 -- Compiled SQL bundle
--- Generated: 2026-02-06 16:09:38
+-- Generated: 2026-02-06 16:51:33
 -- Source folder: C:\ATK_Project\sql_scripts\Silver
 -- Files (12):
 --   mis.Silver_CerereOnline_base.sql
